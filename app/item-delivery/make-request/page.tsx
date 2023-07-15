1054731f68e190a3a9439cff0afa3fd8f7a8b5e8
Vincent Abolarin <vincentabolarin@gmail.com>
2023-07-15 06:19:49 +0100

    set up landing page

---
+++ app/item-delivery/make-request/page.tsx
@@ -0,0 +1,11 @@
+const MakeRequest = () => {
+    return (
+        <>
+            <div className="container">
+                Make Request!
+            </div>
+        </>
+    );
+}
+ 
+export default MakeRequest;
---
+++ app/item-delivery/page.tsx
@@ -0,0 +1,9 @@
+const ItemDelivery = () => {
+    return (
+        <>
+            <div className={styles.container}></div>
+        </>
+    );
+}
+ 
+export default ItemDelivery;
